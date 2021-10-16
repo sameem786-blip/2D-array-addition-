c7a390f2f5fceddba0efd2446cf48906419c1970
sameem786-blip <sameemabbas2002@outlook.com>
2021-10-16 18:19:16 +0500

    initialization

---
+++ script.js
@@ -0,0 +1,31 @@
+const cal = () => {
+    let m1_r1c1 = Number(document.getElementById("m1_r1c1").value);
+    let m1_r1c2 = Number(document.getElementById("m1_r1c2").value);
+    let m1_r1c3 = Number(document.getElementById("m1_r1c3").value);
+    let m1_r2c1 = Number(document.getElementById("m1_r2c1").value);
+    let m1_r2c2 = Number(document.getElementById("m1_r2c2").value);
+    let m1_r2c3 = Number(document.getElementById("m1_r2c3").value);
+    let m1_r3c1 = Number(document.getElementById("m1_r3c1").value);
+    let m1_r3c2 = Number(document.getElementById("m1_r3c2").value);
+    let m1_r3c3 = Number(document.getElementById("m1_r3c3").value);
+
+    let m2_r1c1 = Number(document.getElementById("m2_r1c1").value);
+    let m2_r1c2 = Number(document.getElementById("m2_r1c2").value);
+    let m2_r1c3 = Number(document.getElementById("m2_r1c3").value);
+    let m2_r2c1 = Number(document.getElementById("m2_r2c1").value);
+    let m2_r2c2 = Number(document.getElementById("m2_r2c2").value);
+    let m2_r2c3 = Number(document.getElementById("m2_r2c3").value);
+    let m2_r3c1 = Number(document.getElementById("m2_r3c1").value);
+    let m2_r3c2 = Number(document.getElementById("m2_r3c2").value);
+    let m2_r3c3 = Number(document.getElementById("m2_r3c3").value);
+
+    let m3_r1c1 = Number(document.getElementById("m3_r1c1").value);
+    let m3_r1c2 = Number(document.getElementById("m3_r1c2").value);
+    let m3_r1c3 = Number(document.getElementById("m3_r1c3").value);
+    let m3_r2c1 = Number(document.getElementById("m3_r2c1").value);
+    let m3_r2c2 = Number(document.getElementById("m3_r2c2").value);
+    let m3_r2c3 = Number(document.getElementById("m3_r2c3").value);
+    let m3_r3c1 = Number(document.getElementById("m3_r3c1").value);
+    let m3_r3c2 = Number(document.getElementById("m3_r3c2").value);
+    let m3_r3c3 = Number(document.getElementById("m3_r3c3").value);
+}
