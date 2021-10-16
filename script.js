3d62615fc5763c71546ba5f8bc05ad0c6d708c37
sameem786-blip <sameemabbas2002@outlook.com>
2021-10-16 18:35:59 +0500

    setting

--- script.js
+++ script.js
@@ -19,13 +19,33 @@ const cal = () => {
     let m2_r3c2 = Number(document.getElementById("m2_r3c2").value);
     let m2_r3c3 = Number(document.getElementById("m2_r3c3").value);
 
-    let m3_r1c1 = Number(document.getElementById("m3_r1c1").value);
-    let m3_r1c2 = Number(document.getElementById("m3_r1c2").value);
-    let m3_r1c3 = Number(document.getElementById("m3_r1c3").value);
-    let m3_r2c1 = Number(document.getElementById("m3_r2c1").value);
-    let m3_r2c2 = Number(document.getElementById("m3_r2c2").value);
-    let m3_r2c3 = Number(document.getElementById("m3_r2c3").value);
-    let m3_r3c1 = Number(document.getElementById("m3_r3c1").value);
-    let m3_r3c2 = Number(document.getElementById("m3_r3c2").value);
-    let m3_r3c3 = Number(document.getElementById("m3_r3c3").value);
+    // let m3_r1c1 = Number(document.getElementById("m3_r1c1").value);
+    // let m3_r1c2 = Number(document.getElementById("m3_r1c2").value);
+    // let m3_r1c3 = Number(document.getElementById("m3_r1c3").value);
+    // let m3_r2c1 = Number(document.getElementById("m3_r2c1").value);
+    // let m3_r2c2 = Number(document.getElementById("m3_r2c2").value);
+    // let m3_r2c3 = Number(document.getElementById("m3_r2c3").value);
+    // let m3_r3c1 = Number(document.getElementById("m3_r3c1").value);
+    // let m3_r3c2 = Number(document.getElementById("m3_r3c2").value);
+    // let m3_r3c3 = Number(document.getElementById("m3_r3c3").value);
+
+    let m1 = [
+        [m1_r1c1,m1_r1c2,m1_r1c3],
+        [m1_r2c1,m1_r2c2,m1_r2c3],
+        [m1_r3c1,m1_r3c2,m1_r3c3]
+    ];
+
+    let m2 = [
+        [m2_r1c1,m2_r1c2,m2_r1c3],
+        [m2_r2c1,m2_r2c2,m2_r2c3],
+        [m2_r3c1,m2_r3c2,m2_r3c3]
+    ];
+
+    let m3 = [
+        [m1_r1c1 + m2_r1c1,m1_r1c2 + m2_r1c2,m1_r1c3 + m2_r1c3],
+        [m1_r2c1 + m2_r2c1,m1_r2c2 + m2_r2c2,m1_r2c3 + m2_r2c3],
+        [m1_r3c1 + m2_r3c1,m1_r3c2 + m2_r3c2,m1_r3c3 + m2_r3c3]
+    ];
+
+    console.log("succ")
 }
