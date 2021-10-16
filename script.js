efcd7a4456a4f9bca526cbd7d4b9c6eca741cea8
sameem786-blip <sameemabbas2002@outlook.com>
2021-10-16 23:29:01 +0500

    using map fn to add

--- script.js
+++ script.js
@@ -41,21 +41,26 @@ const cal = () => {
         [m2_r3c1,m2_r3c2,m2_r3c3]
     ];
 
-    let m3 = [
-        [m1_r1c1 + m2_r1c1,m1_r1c2 + m2_r1c2,m1_r1c3 + m2_r1c3],
-        [m1_r2c1 + m2_r2c1,m1_r2c2 + m2_r2c2,m1_r2c3 + m2_r2c3],
-        [m1_r3c1 + m2_r3c1,m1_r3c2 + m2_r3c2,m1_r3c3 + m2_r3c3]
-    ];
+    m1.map(()=>{
+        
+    })
+    
 
-    document.getElementById("m3_r1c1").value = m3[0][1];
-    document.getElementById("m3_r1c2").value = m3[0][2];
-    document.getElementById("m3_r1c3").value = m3[0][3];
-    document.getElementById("m3_r2c1").value = m3[1][1];
-    document.getElementById("m3_r2c2").value = m3[1][2];
-    document.getElementById("m3_r2c3").value = m3[1][3];
-    document.getElementById("m3_r3c1").value = m3[2][1];
-    document.getElementById("m3_r3c2").value = m3[2][2];
-    document.getElementById("m3_r3c3").value = m3[2][3];
+    // let m3 = [
+    //     [m1_r1c1 + m2_r1c1,m1_r1c2 + m2_r1c2,m1_r1c3 + m2_r1c3],
+    //     [m1_r2c1 + m2_r2c1,m1_r2c2 + m2_r2c2,m1_r2c3 + m2_r2c3],
+    //     [m1_r3c1 + m2_r3c1,m1_r3c2 + m2_r3c2,m1_r3c3 + m2_r3c3]
+    // ];
+
+    // document.getElementById("m3_r1c1").value = m3[0][0];
+    // document.getElementById("m3_r1c2").value = m3[0][1];
+    // document.getElementById("m3_r1c3").value = m3[0][2];
+    // document.getElementById("m3_r2c1").value = m3[1][0];
+    // document.getElementById("m3_r2c2").value = m3[1][1];
+    // document.getElementById("m3_r2c3").value = m3[1][2];
+    // document.getElementById("m3_r3c1").value = m3[2][0];
+    // document.getElementById("m3_r3c2").value = m3[2][1];
+    // document.getElementById("m3_r3c3").value = m3[2][2];
 
 
 }
