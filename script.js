6d0f63b5df43fc355b03e29e02b7bd0009caf6c4
sameem786-blip <sameemabbas2002@outlook.com>
2021-10-16 18:56:28 +0500

    script init

--- script.js
+++ script.js
@@ -47,5 +47,15 @@ const cal = () => {
         [m1_r3c1 + m2_r3c1,m1_r3c2 + m2_r3c2,m1_r3c3 + m2_r3c3]
     ];
 
-    console.log("succ")
+    document.getElementById("m3_r1c1").value = m3[0][1];
+    document.getElementById("m3_r1c2").value = m3[0][2];
+    document.getElementById("m3_r1c3").value = m3[0][3];
+    document.getElementById("m3_r2c1").value = m3[1][1];
+    document.getElementById("m3_r2c2").value = m3[1][2];
+    document.getElementById("m3_r2c3").value = m3[1][3];
+    document.getElementById("m3_r3c1").value = m3[2][1];
+    document.getElementById("m3_r3c2").value = m3[2][2];
+    document.getElementById("m3_r3c3").value = m3[2][3];
+
+
 }
